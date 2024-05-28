3c496fdb29d84ffd4200585637452521c9dd3bc4
siyajari samuel <siyajari.samuel@student.moringaschool.com>
2024-05-28 20:03:26 +0300

    Object Inheritance Lab

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,13 @@
 from user import User
 
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        print("Student.__init__ called.")
+        self.knowledge = []
+
+    def learn(self, new_knowledge):
+        print("Student.learn called.")
+        self.knowledge.append(new_knowledge)
+            
+        
